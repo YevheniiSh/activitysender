423b8fc177f52ecc13bb851181deaa2092fd5c7f
yevheniishc <yevheniishc@softwareplanet.uk.com>
2018-02-08 14:10:23 +0200

    add screenshots example

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,8 +12,8 @@ import java.util.stream.Collectors;
 public class App {
     public static void main(String[] args) throws IOException {
         CloseableHttpClient httpClient = HttpClients.createDefault();
-        HttpPost uploadFile = new HttpPost("http://interlink.staging.taho2.interlink-ua.com/api/v1/application_activities/statistic/c7d8c0a5-7549-4f87-8e64-45325d587fcc");
-        uploadFile.setHeader("X-CSRF-TOKEN", "efc20351-9e6f-4080-b3b2-5b32c82fa783");
+        HttpPost uploadFile = new HttpPost("http://interlink.taho2-loc.com:8080/api/v1/application_activities/statistic/auto_id"); // add auto_id
+        uploadFile.setHeader("X-CSRF-TOKEN", ""); // add token
         MultipartEntityBuilder builder = MultipartEntityBuilder.create();
         File f = new File("/home/yevheniishc/meta.xml");
         builder.addBinaryBody(
@@ -21,6 +21,20 @@ public class App {
                 new FileInputStream(f),
                 ContentType.APPLICATION_OCTET_STREAM,
                 f.getName()
+        );
+        File screenshotA = new File("/home/yevheniishc/screenshot_20180116-161100-A-original.png");
+        builder.addBinaryBody(
+                "screenshot",
+                new FileInputStream(screenshotA),
+                ContentType.APPLICATION_OCTET_STREAM,
+                screenshotA.getName()
+        );
+        File screenshotB = new File("/home/yevheniishc/screenshot_20180116-161100-B-original.png");
+        builder.addBinaryBody(
+                "screenshot",
+                new FileInputStream(screenshotB),
+                ContentType.APPLICATION_OCTET_STREAM,
+                screenshotB.getName()
         );
 
         HttpEntity multipart = builder.build();
